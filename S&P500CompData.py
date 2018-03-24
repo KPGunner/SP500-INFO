9e8eccf3184fedfc2b2ee900ea85e0ab2197891e
KPGunner <31373828+KPGunner@users.noreply.github.com>
2018-03-24 13:46:21 -0400

    Add files via upload

---
+++ S&P500CompData.py
@@ -0,0 +1,14 @@
+import quandl
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
+
+
+df = quandl.get("YALE/SPCOMP", authtoken="XPC5T_rCEEd5aNnys5rn", end_date="2018-02-23")
+
+print(df.tail(5))
+
+df.plot()
+plt.title('S&P Composite Data')
+plt.show()
